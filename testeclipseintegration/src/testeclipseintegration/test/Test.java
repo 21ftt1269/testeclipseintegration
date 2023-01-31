c884a351c58e1b064005d752438e45fc19a2ecab
wafiq naqiuddin <21FTT1269@student.pb.edu.bn>
2023-01-31 15:30:45 +0800

    commit abc

--- testeclipseintegration/src/testeclipseintegration/test/Test.java
+++ testeclipseintegration/src/testeclipseintegration/test/Test.java
@@ -3,6 +3,8 @@ package testeclipseintegration.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("GitHub");
+		ClassA ClassA = new ClassA("ABCDEF");
+				System.out.println(ClassA.getAbc());
 	}
 
 }
